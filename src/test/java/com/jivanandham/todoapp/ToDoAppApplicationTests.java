55d531437ec13a9df6f9f69fa052d9223488e457
Jeeva <jivanandham@gmail.com>
2021-11-05 20:01:11 +0530

    first first gitcode

---
+++ src/test/java/com/jivanandham/todoapp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jivanandham.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
